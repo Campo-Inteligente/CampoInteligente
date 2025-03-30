71651501eb9c6817b1b58440a939336fc8efcbf4
giselegomes431 <belagisa14@gmail.com>
2025-03-30 12:28:54 -0300

    create: NavbarVerde.js

--- pages/contato.js
+++ pages/contato.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavbarVerde';
 import Footer from '../components/Footer';
 import styles from '../styles/Contato.module.css';
 
