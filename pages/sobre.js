1d901699fac4d65b7ceb8d9b08f9e360446cec88
Fabio <202210091@uesb.edu.br>
2025-03-31 17:45:24 -0300

    Atualizações no layout: ajustes no Navbar, cards da equipe e links do LinkedIn

--- pages/sobre.js
+++ pages/sobre.js
@@ -20,52 +20,52 @@ export default function Sobre() {
       imagem: "/imagens/arthur.jpg"
     },
     {
-      cargo: "Desenvolvimento Mobile",
-      nome: "Bruna Galiza",
-      descricao: "Técnica em Informática pelo Instituto Federal de Educação, Ciência e Tecnologia Baiano – Campus Santa Inês. Possui experiência prática em projetos voltados para tecnologia, comunicação e atendimento ao público.",
+      cargo: "Analista de Cloud",
+      nome: "Bruna Queiroz",
+      descricao: "Técnica em Informática pelo IF Baiano, com experiência em comunicação e atendimento ao público.",
       imagem: "/imagens/bruna.jpg"
     },
     {
       cargo: "Desenvolvedor front end",
       nome: "Fabio Santos",
-      descricao: "Técnico em Informática pelo IFBA, graduando em Sistemas de Informação pela UESB. Possui experiência em bancos de dados, desenvolvimento web, flutter e linguagens como Java e Python.",
+      descricao: "Técnico em Informática pelo IFBA, graduando em Sistemas de Informação pela UESB",
       imagem: "/imagens/fabio.jpeg"
     },
     {
       cargo: "Desenvoldedora Fron end",
       nome: "Gisele Gomes",
-      descricao: "Graduando em Sistemas de Informação pela Universidade Estadual da Bahia, tem experiência com desenvolvimento WEB e front end",
+      descricao: "Graduando em Sistemas de Informação pela UESB e desenvolvedora front-end.",
       imagem: "/imagens/gisele.jpg"
     },
     {
       cargo: "DEsenvolvedor mobile",
       nome: "João Victor",
-      descricao: "Graduando em Sistemas de Informações pela Universidade Estadual do Sudoeste da Bahia.",
+      descricao: "Graduando em Sistemas de Informações pela UESB.",
       imagem: "/imagens/joao.jpg"
     },
     {
       cargo: "Desenvolvedor back end",
       nome: "Juan Pablo",
-      descricao: "Técnico em Informática pelo IFBA e graduando em Sistemas de Informação na UESB, com experiência em back-end e automação. Especialista em soluções inovadoras com Python, focadas em eficiência e análise de dados.",
+      descricao: "Técnico em Informática pelo IFBA e graduando em SI na UESB.",
       imagem: "/imagens/juan.jpg"
     },
     {
       cargo: "Gerente de projeto",
       nome: "Marcos Morais",
-      descricao: "Dinâmico Gerente de Projetos de TI com mais de 15 anos de experiência, especializado em gerenciamento ágil e desenvolvimento de software usando tecnologias como C#, Python, PHP e bancos de dados relacionais, como Oracle e SQL Server. Bacharel em Sistemas de Informação, pós-graduado em Gestão de Tecnologia da Informação e Mestre em Ciência da Computação.",
+      descricao: "Dinâmico Gerente de Projetos de TI com mais de 15 anos de experiência.",
       imagem: "/imagens/marcos.jpg"
     }
   ];
 
   return (
+    
     <div className={styles.sobreContainer}>
       <Head>
         <title>Sobre Nós | Campo Inteligente</title>
         <meta name="description" content="Conheça nossa equipe e nossa missão" />
       </Head>
-      
       <Navbar />
-        
+   
       <main>
         <section className={styles.intro}>
           <div className={styles.texto}>
@@ -89,50 +89,62 @@ export default function Sobre() {
         </section>
         
         <section className={styles.blocos}>
-          <div className={`${styles.bloco} ${styles.missao}`}>
-             <Image
-             src="/imagens/missao.png"
-             alt="missão"
-             width={50}
-             height={50}
-             />
-            <h2>Missão</h2>
-            <p>
-            Democratizar o acesso à tecnologia e à inteligência artificial para agricultores
-                familiares, oferecendo informações estratégicas que otimizam a produtividade,
-                reduzem riscos e promovem uma agricultura mais eficiente, sustentável e rentável.
-            </p>
-          </div>
-          
-          <div className={`${styles.bloco} ${styles.visao}`}>
-          <Image
-             src="/imagens/visão.png"
-             alt="missão"
-             width={50}
-             height={50}
-             />
-            <h2>Visão</h2>
-            <p>
-            Ser a principal plataforma de suporte inteligente para a agricultura familiar,
-                conectando tecnologia e inovação ao campo para transformar a tomada de decisões
-                e impulsionar um agro mais produtivo, acessível e sustentável.
-            </p>
-          </div>
-          
-          <div className={`${styles.bloco} ${styles.valores}`}>
-          <Image
-             src="/imagens/valores.png"
-             alt="missão"
-             width={50}
-             height={50}
-             />
-            <h2>Valores</h2>
-            <p>
-                Inovação, Acessibilidade  Sustentabilidade,Confiabilidade
-                estratégicas no campo e Empoderamento.
-            </p>
-          </div>
-        </section>
+  <div className={`${styles.bloco} ${styles.missao}`}>
+    <div className={styles.tituloComIcone}>
+      <Image
+        src="/imagens/missao.png"
+        alt="missão"
+        width={50} 
+        height={50}
+        className={styles.iconeBloco}
+      />
+      <h2>Missão</h2>
+    </div>
+    <p>
+      Democratizar o acesso à tecnologia e à inteligência artificial para agricultores
+      familiares, oferecendo informações estratégicas que otimizam a produtividade,
+      reduzem riscos e promovem uma agricultura mais eficiente, sustentável e rentável.
+    </p>
+  </div>
+  
+  <div className={`${styles.bloco} ${styles.visao}`}>
+    <div className={styles.tituloComIcone}>
+      <Image
+        src="/imagens/visão.png"
+        alt="visão"
+        width={50}
+        height={50}
+        className={styles.iconeBloco}
+      />
+      <h2>Visão</h2>
+    </div>
+    <p>
+      Ser a principal plataforma de suporte inteligente para a agricultura familiar,
+      conectando tecnologia e inovação ao campo para transformar a tomada de decisões
+      e impulsionar um agro mais produtivo, acessível e sustentável.
+    </p>
+  </div>
+  
+  <div className={`${styles.bloco} ${styles.valores}`}>
+    <div className={styles.tituloComIcone}>
+      <Image
+        src="/imagens/valores.png"
+        alt="valores"
+        width={50}
+        height={50}
+        className={styles.iconeBloco}
+      />
+      <h2>Valores</h2>
+    </div>
+    <p>
+•	Inovação
+•	Acessibilidade
+•	Sustentabilidade
+•	Confiabilidade Estratégicas no campo e Empoderamento
+
+    </p>
+  </div>
+</section>
         
         <section className={styles.equipe}>
   <h2>Nossa equipe</h2>
@@ -153,14 +165,21 @@ export default function Sobre() {
           <h3>{membro.nome}</h3>
           <p className={styles.cargo}>{membro.cargo}</p>
           <p className={styles.descricao}>{membro.descricao}</p>
-          <a href="#" className={styles.linkedin}>
-            <img 
-              src="/imagens/linkedin.png" 
-              alt="LinkedIn" 
-              width={20}
-              height={20}
-            />
-          </a>
+          <a 
+  href="https://www.linkedin.com" 
+  target="_blank" 
+  rel="noopener noreferrer" 
+  className={styles.linkedinBtn}
+>
+  <Image 
+    src="/imagens/linkedin.png" 
+    alt="LinkedIn" 
+    width={24}
+    height={24}
+    className={styles.linkedinIcon}
+  />
+</a>
+
         </div>
       </div>
     ))}
