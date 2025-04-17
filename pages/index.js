3b851ab2f14844088409b1ef71b235d9a8eccab6
Marcos Morais <mmstec@gmail.com>
2025-04-17 15:37:14 -0300

    Atualizando index.js com alterações mais recentes

--- pages/index.js
+++ pages/index.js
@@ -200,7 +200,7 @@ export default function Home() {
                     o plantio, o manejo das lavouras e a colheita. Tudo isso na
                     palma da sua mão, direto pelo WhatsApp, para que você tome
                     decisões estratégicas e maximize seus lucros com segurança e
-                    praticidade!
+                    praticidade! 
                   </motion.p>
                 </div>
               </motion.div>
