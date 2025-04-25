6e03b6f516c6fee9db07682d2c92b39c02ea23d3
Marcos Morais <mmstec@gmail.com>
2025-04-25 15:25:05 -0300

    resolvendo critica sobre fontes personalizadas no arquivo pages/_document.js do vercel

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,16 @@ export default function Document() {
   return (
     <Html lang="pt">
       <Head>
-        <link rel="icon" href="/icone.ico" sizes="any" />
+        {/* Ícones */}
+        <link rel="icon" href="/icone.ico" />
         <link rel="icon" type="image/png" href="/icon.png" />
 
-      {/* adicionado o link da font no document.js, isso não pode ser adicionado em paginas internas diretamente...
-       O correto é adicionar no "template/layout" do nextJs, que usando a rota "pages" é o arquivo _documentjs.. 
-       Isso vale para outros imports também */}
+        {/* Fontes personalizadas - Adicionado no _document.js para uso global */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
           rel="stylesheet"
-        /> 
-        
+        />
       </Head>
       <body>
         <Main />
