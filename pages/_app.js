9cc4a68061d97e5f16d90e833b7f7829bceea4ba
giselegomes431 <belagisa14@gmail.com>
2025-06-09 17:42:21 -0300

    fix: exclusão da sobre.css que estava gerando bugs no site

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import '../styles/sobre.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
